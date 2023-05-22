b586a65898b9e4a0e8b4c3606dc2af63aa5bea74
bansidhaduk <patel.bansi1205@gmail.com>
2023-05-22 15:18:19 -0400

    Initial commit

---
+++ src/main/java/com/javaBasics/E11.java
@@ -0,0 +1,16 @@
+// Write a program to print even numbers between 23 and 57, each number should be
+//printed in a separate row.
+
+package com.javaBasics;
+
+public class E11 {
+    public static void main(String[] args) {
+
+        System.out.println("Even numbers between 23 and 57 are: ");
+
+        for(int number = 23; number <= 57; number++) {
+            if (number % 2 == 0)
+                System.out.println(number);
+        }
+    }
+}
---
+++ src/main/java/com/javaBasics/E12.java
@@ -0,0 +1,36 @@
+// Write a program to check if a given number is prime or not
+
+package com.javaBasics;
+import java.util.Scanner;
+
+public class E12 {
+    public static void main(String args[])
+    {
+        boolean isPrime = true;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to check if it is prime or not : ");
+        int number = input.nextInt();
+
+        if (number < 2)
+            isPrime = false;
+        else {
+            for(int i = 2; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if(isPrime)
+            System.out.println(number + " is a Prime Number");
+        else
+            System.out.println(number + " is not a Prime Number");
+
+//        String result = isPrime ? "Prime" : "not Prime";
+//        System.out.println (number + " is  " + result);
+
+        input.close();
+    }
+}
