61b2f8bda18458f9cbdc38612cf4c0d574cf21bd
bansidhaduk <patel.bansi1205@gmail.com>
2023-05-25 20:32:28 -0400

    Initial commit

--- src/main/java/com/vector/Ex01.java
+++
@@ -1,9 +0,0 @@
-//Write a method in the Vector class that calculates and returns the magnitude (length) of a vector. The magnitude of a vector can be calculated by taking the square root of the sum of the squares of its elements.
-
-package com.vector;
-
-public class Ex01 {
-    public static void main(String[] args) {
-
-    }
-}
